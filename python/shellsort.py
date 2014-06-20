18de1ea9462fa2a11043fe6b5bc772743319dcaf
James Liu <jamesliu96@163.com>
2014-06-20 22:59:42 +0800

    Add new python demo code

---
+++ python/shellsort.py
@@ -0,0 +1,17 @@
+def shellSort(A):
+    def getCols(n):
+        cols = [1]
+        val = 1
+        while val < n:
+            val = int(val * 2.2)
+            cols.insert(0, val)
+        return cols
+    for h in getCols(len(A)):
+        for i in range(h, len(A)):
+            cur = A[i]
+            j = i
+            while j >= h and A[j - h] > cur:
+                A[j] = A[j - h]
+                j -= h
+            A[j] = cur
+    return A
